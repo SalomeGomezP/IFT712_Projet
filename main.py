51b4e289518735e023dd555a72e9f26d83ba1cd1
Julien <jbrochier@ensc.fr>
2019-11-19 18:46:39 -0500

    visualisation nouvelle bdd

--- main.py
+++ main.py
@@ -8,21 +8,13 @@ Created on Fri Nov 15 11:23:37 2019
 import csv
 import pandas as pd
 
-# Import
-# =============================================================================
-# t_train=[]
-# x_train=[]
-# with open("data/train.csv") as file:
-#     reader = csv.reader(file)
-#     titles = next(reader)
-#     for row in reader :
-#         t_train.append(row[1])
-#         x_train.append([:1:])
-# =============================================================================
-data = pd.read_csv("data/train.csv")
-dataT =data['Survived']
+# Import & format
+data = pd.read_csv("data/train/train.csv")
+dataT =data['AdoptionSpeed']
 t_train=list(pd.Series(dataT.values.tolist()))
-dataX=data.drop('Survived',axis=1)
+dataX=data.drop(['AdoptionSpeed','Name','Description'],axis=1)
 x_train=list(pd.Series(dataX.values.tolist()))
 
 #Visualization
+for col in dataX :
+    dataX.hist(column=col)
