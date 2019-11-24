70adb2ce1eb674c061aaea8a2e0f3899a371d56e
Julien <jbrochier@ensc.fr>
2019-11-23 20:24:53 -0500

    creation LR

---
+++ logistic_regression.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 19:03:12 2019
+
+@author: Julien
+"""
+
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+class logReg():
+    
+    def __init__(self,X_train, T_train):
+        self.clf = LogisticRegression(solver='liblinear').fit(X_train, T_train)
+        self.check1 = 1
+    
+    def predict(self,X):
+        return self.clf.predict(X)
+    
+    def fit(self,X_train,T_train):
+        self.clf = LogisticRegression(solver='liblinear').fit(X_train, T_train)
+
+    def train(self, X_train,T_train, X_test,T_test):     
+        T_train_p = self.predict(X_train)
+        self.T_train_p=T_train_p
+        T_test_p = self.predict(X_test)
+        Err_train = np.int32(T_train_p!=T_train)
+        Err_test = np.int32(T_test_p!=T_test)
+        
+        err_train = np.sum(Err_train)/len(X_train)*100
+        err_test = np.sum(Err_test)/len(X_test)*100
+        return[err_train,err_test]
+    
+    
--- main.py
+++ main.py
@@ -5,16 +5,32 @@ Created on Fri Nov 15 11:23:37 2019
 @author: Julien
 """
 
-import csv
+import numpy as np
 import pandas as pd
+from logistic_regression import logReg
 
 # Import & format
 data = pd.read_csv("data/train/train.csv")
-dataT =data['AdoptionSpeed']
-t_train=list(pd.Series(dataT.values.tolist()))
-dataX=data.drop(['AdoptionSpeed','Name','Description','RescuerID','PetID'],axis=1)
-x_train=list(pd.Series(dataX.values.tolist()))
+msk = np.random.rand(len(data)) < 0.8
+data_train=data[msk]
+data_test=data[~msk]
+
+t_train=list(pd.Series(data_train['AdoptionSpeed'].values.tolist()))
+columns_to_drop=['AdoptionSpeed','Name','Description','RescuerID','PetID']
+x_train=list(pd.Series(data_train.drop(columns_to_drop,axis=1).values.tolist()))
+
+t_test=list(pd.Series(data_test['AdoptionSpeed'].values.tolist()))
+x_test=list(pd.Series(data_test.drop(columns_to_drop,axis=1).values.tolist()))
 
 #Visualization
-for col in dataX :
-    dataX.hist(column=col)
+# =============================================================================
+# for col in dataX :
+#     dataX.hist(column=col)
+# =============================================================================
+    
+LR=logReg(x_train,t_train)
+[err_train,err_test]=LR.train(x_train,t_train,x_test,t_test)
+
+prediction_train = LR.T_train_p
+print('Erreur train = ', err_train, '%')
+print('Erreur test = ', err_test, '%')
