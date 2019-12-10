d7a89357d0f6f6bdeb6198e68fb2f53fc89c15f9
SalomeGomezP <36883497+SalomeGomezP@users.noreply.github.com>
2019-12-10 16:31:34 -0500

    recherche hyper params

--- MLP.py
+++ MLP.py
@@ -7,13 +7,14 @@ Created on Tue Dec 10 14:37:32 2019
 
 import numpy as np
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
 class MLP:
     def __init__(self):
         self.mlp=None
 
-    def training(self, X, t):
-        mlp=MLPClassifier()
+    def training(self, X, t, params):
+        mlp=MLPClassifier(params)
         mlp.fit(X, t)  
         self.mlp=mlp
         
@@ -25,7 +26,9 @@ class MLP:
         
     def launch(self,x_train, x_test, t_train, t_test) :
         
-        self.training(x_train, t_train)
+        params=self.search_hyperparams(x_train,t_train)
+        
+        self.training(x_train, t_train, params)
         
         t_train_prediction=self.prediction(x_train)
         t_test_prediction=self.prediction(x_test)
@@ -42,3 +45,34 @@ class MLP:
         number_error=number_error/len(t)           
         return number_error
     
+    def search_hyperparams(self,x,t):
+        
+        mlp = MLPClassifier(max_iter=100)
+        
+        #generation hidden layer(s)
+        array=[]
+        for  i in range (69,100,10):
+            list_one_layer=list()          
+            list_one_layer.append(i)
+            array.append(tuple(list_one_layer))
+            
+            for j in range (69,100,10):
+                list_two_layer=list()
+                list_two_layer.append(i)
+                list_two_layer.append(j)
+                array.append(tuple(list_two_layer))
+        
+        parameter_space = {
+        'hidden_layer_sizes': array,
+        'activation': ['tanh', 'relu'],
+        'solver': ['sgd', 'adam'],
+        'alpha': [0.0001,0.005, 0.001,0.05, 0.01],
+        }
+        
+        search = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+        search.fit(x,t)
+        
+        print('Best parameters found:\n', search.best_params_)
+        print('Loss associated :\n', search.best_score_)
+        
+        return search.best_params_
